6061e77d6145aa5974d32de41c0b9a932e336ae2
Antony O'Neill <antony@boclips.com>
2019-05-10 15:37:11 +0100

    Add typescript compilation
    [#165905289]

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,23 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
-    filename: 'main.js',
+    filename: 'main.ts',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
+    ]
   }
 };
