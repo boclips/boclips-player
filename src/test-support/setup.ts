bbcfce9e17ee553a6fd5d34e63620b6d52751d9a
Antony O'Neill <antony@boclips.com>
2019-05-13 16:34:28 +0100

    Implement some test-support for jsdom blackholes
    [#165964348]

--- src/test-support/setup.ts
+++ src/test-support/setup.ts
@@ -1,4 +1,21 @@
-// jsdom does not support text tracks in videos
-Object.defineProperty(HTMLVideoElement.prototype, 'textTracks', {
-  value: { mode: 'disabled' },
+// @ts-ignore
+window.TextTrack = jest.fn();
+
+if (typeof HTMLElement.prototype.insertAdjacentElement === 'undefined') {
+  Object.defineProperty(HTMLElement.prototype, 'insertAdjacentElement', {
+    value(_, element) {
+      this.appendChild(element);
+    },
+  });
+}
+
+Object.defineProperty(HTMLMediaElement.prototype, 'load', {
+  value: jest.fn(),
 });
+
+console.log = s => {
+  process.stdout.write(s + '\n');
+};
+console.debug = s => {
+  process.stdout.write(s + '\n');
+};
