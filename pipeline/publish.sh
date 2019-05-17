e62dbb81c6793bd5610e1106da54fe1e23ee8cd7
Antony O'Neill <antony@boclips.com>
2019-05-17 17:46:41 +0100

    Fix publish script

--- pipeline/publish.sh
+++ pipeline/publish.sh
@@ -11,4 +11,4 @@ npm run build
 npm --no-git-tag-version -f version "$(< ../version/version)"
 )
 
-cp -R ${app}/dist/ ${app}/package.json ${app}/README.md dist/
+cp -R ${app}/dist/ ${app}/package.json ${app}/README.adoc dist/
