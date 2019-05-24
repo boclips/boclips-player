a0013c766dd09b9b4a2f6d699d5aa7a1f58367f8
Antony O'Neill <antony@boclips.com>
2019-05-24 12:07:01 +0100

    produce less in esm too

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # Produce the ES5/CommonJS into /lib
 tsc
 
@@ -13,15 +15,21 @@ webpack
 
 # Copy the LESS assets out into /lib
 
-SRC=src
-LIB=lib
+SRC=$SCRIPT_DIR/../src
+LIB=$SCRIPT_DIR/../lib
+ESM=$SCRIPT_DIR/../esm
 
+# cd to src so it doesn't appear in the find output
 cd $SRC
 
 LESS_FILES=$(find . -name \*.less)
 
 for FILE in $LESS_FILES; do
     DIR_PATH=$(dirname $FILE)
+
     mkdir -p $LIB/$DIR_PATH
     cp $FILE $LIB/$FILE
+
+    mkdir -p $ESM/$DIR_PATH
+    cp $FILE $ESM/$FILE
 done
