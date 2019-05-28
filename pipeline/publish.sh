a54c7e5093d7087c2f06af8f237d37823c2a8406
Antony O'Neill <antony@boclips.com>
2019-05-28 11:12:45 +0100

    Generate release data whilst in home dir
    We were referring to the wrong directory.
    
    Signed-off-by: Andrew Bruce <me@andrewbruce.net>

--- pipeline/publish.sh
+++ pipeline/publish.sh
@@ -4,18 +4,17 @@ set -x -e
 
 app=source
 release_name="v$(< version/version)"
-(
-cd ${app}
-npm ci
-
-npm run build
-npm version "$(< ../version/version)" \
-    --no-git-tag-version \
-    --force
 echo "$release_name" \
     > release/name
 echo "This is $release_name" \
     > release/notes
+(
+cd ${app}
+npm ci
+npm run build
+npm version "$(< ../version/version)" \
+    --no-git-tag-version \
+    --force
 )
 
 cp -R ${app}/dist/ ${app}/lib/ ${app}/esm/ ${app}/package.json ${app}/README.md dist/
