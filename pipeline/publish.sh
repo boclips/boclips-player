2d6245103094708484a114a397dc9bf500271aee
Antony O'Neill <antony@boclips.com>
2019-05-28 11:07:04 +0100

    npm no longer tags
    Output some release information into files consumable by Concourse
    
    Signed-off-by: Andrew Bruce <me@andrewbruce.net>

--- pipeline/publish.sh
+++ pipeline/publish.sh
@@ -3,13 +3,19 @@
 set -x -e
 
 app=source
+release_name="v$(< version/version)"
 (
 cd ${app}
 npm ci
 
 npm run build
-npm -f version "$(< ../version/version)"
-git push --tags origin master
+npm version "$(< ../version/version)" \
+    --no-git-tag-version \
+    --force
+echo "$release_name" \
+    > release/name
+echo "This is $release_name" \
+    > release/notes
 )
 
 cp -R ${app}/dist/ ${app}/lib/ ${app}/esm/ ${app}/package.json ${app}/README.md dist/
