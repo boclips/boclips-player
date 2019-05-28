5c4a17a0ddc1736bdb073441535f0cabf2f8965c
Antony O'Neill <antony@boclips.com>
2019-05-28 11:17:58 +0100

    Use npm --prefix instead of cd
    Signed-off-by: Andrew Bruce <me@andrewbruce.net>

--- pipeline/publish.sh
+++ pipeline/publish.sh
@@ -3,18 +3,18 @@
 set -x -e
 
 app=source
-release_name="v$(< version/version)"
+version="$(< version/version)"
+release_name="v$version"
 echo "$release_name" \
     > release/name
 echo "This is $release_name" \
     > release/notes
-(
-cd ${app}
-npm ci
-npm run build
-npm version "$(< ../version/version)" \
+npm="npm --prefix $app"
+
+$npm ci
+$npm run build
+$npm version "$version" \
     --no-git-tag-version \
     --force
-)
 
 cp -R ${app}/dist/ ${app}/lib/ ${app}/esm/ ${app}/package.json ${app}/README.md dist/
