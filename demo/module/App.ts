93cd43f8dcb55be6439c4014128618304fcc6bb5
Benjamin Gröhbiel <ben@boclips.com>
2019-05-31 11:46:21 +0100

    Improve documentation to use player as module
    [#166294195]
    
    Signed-off-by: Antony O'Neill <antony@boclips.com>

---
+++ demo/module/App.ts
@@ -0,0 +1,19 @@
+import { PlayerFactory } from '../../lib/index';
+
+const playerContainer = document.createElement('div');
+playerContainer.id = 'player-container';
+
+document.body.appendChild(playerContainer);
+
+const player = PlayerFactory.get(playerContainer);
+
+player.loadVideo(
+  'https://teachers.staging-boclips.com/v1/videos/5c542ab85438cdbcb56ddceb',
+).then(() => {
+  return player.play();
+}).then(() => {
+  setTimeout(() => {
+    player.pause()
+  }, 10000);
+});
+
