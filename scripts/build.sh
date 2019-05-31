46a7c37b2435958e62262a7937e095e8371529fc
Antony O'Neill <antony@boclips.com>
2019-05-31 14:48:57 +0100

    Make build script clearer

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,20 +4,21 @@ set -xe
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+SRC=$SCRIPT_DIR/../src
+LIB=$SCRIPT_DIR/../lib
+ESM=$SCRIPT_DIR/../esm
+
 # Produce the ES5/CommonJS into /lib
-tsc --target ES5 --outDir lib
+tsc --target ES5 --outDir $LIB
 
 # Produce the ES6/ESModule into /esm
-tsc --target ES6 --outDir esm
+tsc --target ES6 --outDir $ESM
 
 # Produce the bundled ES5 into /dist
 webpack
 
 # Copy the LESS assets out into /lib
 
-SRC=$SCRIPT_DIR/../src
-LIB=$SCRIPT_DIR/../lib
-ESM=$SCRIPT_DIR/../esm
 
 # cd to src so it doesn't appear in the find output
 cd $SRC
