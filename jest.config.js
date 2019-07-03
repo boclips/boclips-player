1fe1ca2b04130bdb40846f7778c747ee72da02cc
Antony O'Neill <antony@boclips.com>
2019-07-03 16:58:22 +0100

    Fix jest config when lib,dist,esm exist

--- jest.config.js
+++ jest.config.js
@@ -85,7 +85,7 @@ module.exports = {
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  // modulePathIgnorePatterns: [],
+  modulePathIgnorePatterns: ['/lib', '/esm', '/dist'],
 
   // Activates notifications for test results
   // notify: false,
@@ -118,9 +118,7 @@ module.exports = {
   // rootDir: null,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  // roots: ['./src'],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
