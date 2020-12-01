f9be1c29bd29e601304137f6ecf2e71210c76df1
Eva Varga <eva@boclips.com>
2020-12-01 10:12:54 +0000

    Fix importing from webpack-merge after upgrade

--- webpack-config/webpack.analyse.js
+++ webpack-config/webpack.analyse.js
@@ -1,7 +1,7 @@
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin;
 
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const production = require('./webpack.prod.js');
 
 module.exports = merge(production, {
