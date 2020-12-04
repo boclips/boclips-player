3e338607343f8e3d20cfa33e6bf03b22c48b325f
Remigiusz Oleszczuk <remy@boclips.com>
2020-12-04 10:51:39 +0000

    Oops! Implement ConsoleLogger#error correctly
    Signed-off-by: Andrew Bruce <me@andrewbruce.net>

--- src/ConsoleLogger.ts
+++ src/ConsoleLogger.ts
@@ -2,7 +2,7 @@ import { Logger } from './Logger';
 
 export class ConsoleLogger implements Logger {
   public error = (...args: any) => {
-    console.log(args);
+    console.error(args);
   };
 
   public warn = (...args: any) => {
